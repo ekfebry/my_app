438629691fdc780e656f6649ab4055071b21990e
Eka febriyanti kholifah <ekfbryntikh@gmail.com>
2023-12-30 10:07:08 +0700

    perubahan 3

--- login/views.py
+++ login/views.py
@@ -1,14 +1,26 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Beranda, Produk
 
 def login(request):
   template = loader.get_template('beranda.html')
   return HttpResponse(template.render())
 
 def beranda(request):
+  data = Beranda.objects.all()
   template = loader.get_template('beranda.html')
-  return HttpResponse(template.render())
+  context ={
+    'menu': 'Macam-Macam Jenis Sablon yang Tersedia di Eka Sablon',
+    'beranda' : data
+  }
+  return HttpResponse(template.render(context, request))
 
 def produk(request):
+  data = Produk.objects.all()
   template = loader.get_template('produk.html')
-  return HttpResponse(template.render())
+  context = {
+    'list': 'Macam-Macam Produk dan Deskripsi :',
+    'produk' : data
+  }
+  return HttpResponse(template.render(context, request))
